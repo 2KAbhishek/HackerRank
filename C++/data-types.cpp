58cf944dae3373b6b2bf144843e27f5aa99da93e
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-06-12 00:06:39 +0530

    Add data-types.cpp

---
+++ C++/data-types.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int n;
+    long ln;
+    char ch;
+    float fl;
+    double d;
+    cin >> n >> ln >> ch >> fl >> d;
+    printf ("%d \n%ld \n%c \n%.3f \n%.9lf \n", n, ln, ch, fl, d);
+    return 0;
+}
+
