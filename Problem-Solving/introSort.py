ce354b9c7d5f93c7cabadcccac8a6c9fe09e67f8
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-06-12 00:07:09 +0530

    Add introSort.py

---
+++ Problem-Solving/introSort.py
@@ -0,0 +1,20 @@
+import os
+
+def introTutorial(V, arr):
+    return arr.index(V)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    V = int(input())
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = introTutorial(V, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
