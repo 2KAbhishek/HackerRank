7d7df32b34100de964ff0e0768265ee5d9722b73
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-06-12 00:06:39 +0530

    Add pointer.cpp

---
+++ C++/pointer.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+void update(int *a,int *b) {
+    int temp1, temp2;
+    temp1 = (*a) + (*b);
+    temp2 = abs((*a) -(*b));
+    (*a) = temp1;
+    (*b) = temp2;
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
