0de333f40a3e4cfd65f1015cf734a28c0df155aa
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-06-12 00:06:47 +0530

    Add DateAndTime.java

---
+++ Java/DateAndTime.java
@@ -0,0 +1,8 @@
+import java.time.LocalDate;
+class Result {
+
+    public static String findDay(int month, int day, int year) {
+        return LocalDate.of(year, month, day).getDayOfWeek().name();
+    }
+
+}
