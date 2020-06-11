62354fd92b008b2444ef4aed476741ef8aed5aa9
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-06-12 00:07:31 +0530

    Add str-formatting.py

---
+++ Python/str-formatting.py
@@ -0,0 +1,11 @@
+def print_formatted(n):
+    w = len(bin(n)[2:])
+    for i in range(1,n+1):
+        print (str(i).rjust(w,' '),\
+        str(oct(i)[2:]).rjust(w,' '),\
+        str(hex(i)[2:].upper()).rjust(w,' '),\
+        str(bin(i)[2:]).rjust(w,' '))
+
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
