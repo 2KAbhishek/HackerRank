77d81cb5996e11fa496f1f17227494023bbb3931
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-06-12 00:07:31 +0530

    Add matrix-script.py

---
+++ Python/matrix-script.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+import re
+
+n, m = map(int, input().split())
+a, b = [], ""
+for _ in range(n):
+    a.append(input())
+
+for z in zip(*a):
+    b += "".join(z)
+
+print(re.sub(r"(?<=\w)([^\w]+)(?=\w)", " ", b))
