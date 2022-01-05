984fc4a872b097ae1bc0695f4452a90a6a25c47d
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-01-05 16:47:30 +0530

    Add array_del.rb

--- Ruby/array_del.rb
+++ Ruby/array_del.rb
@@ -1,15 +1,15 @@
 def end_arr_delete(arr)
-   arr.pop 
+  arr.pop
 end
 
 def start_arr_delete(arr)
-   arr.shift 
+  arr.shift
 end
 
 def delete_at_arr(arr, index)
-   arr.delete_at(index) 
+  arr.delete_at(index)
 end
 
 def delete_all(arr, val)
-   arr.delete(val) 
+  arr.delete(val)
 end
