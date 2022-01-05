a2a35174208762475ca7a55aa096c38698d85de2
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-01-05 16:47:30 +0530

    Add hash_ops.rb

--- Ruby/hash_ops.rb
+++ Ruby/hash_ops.rb
@@ -1,3 +1,3 @@
-hackerrank.store(543121, 100)
-hackerrank.keep_if {|key, value| key.is_a? Integer}
-hackerrank.delete_if {|key, value| key.even?}
+hackerrank.store(543_121, 100)
+hackerrank.keep_if { |key, _value| key.is_a? Integer }
+hackerrank.delete_if { |key, _value| key.even? }
