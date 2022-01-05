faf9fcb074b47ddcc7cfe3e2f2bd36d975d33e23
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-01-05 16:47:30 +0530

    Add array_add.rb

--- Ruby/array_add.rb
+++ Ruby/array_add.rb
@@ -1,15 +1,15 @@
 def end_arr_add(arr, element)
-    arr.push(element)
+  arr.push(element)
 end
 
 def begin_arr_add(arr, element)
-    arr.unshift(element)
+  arr.unshift(element)
 end
 
 def index_arr_add(arr, index, element)
-    arr.insert(index, element)
+  arr.insert(index, element)
 end
 
 def index_arr_multiple_add(arr, index)
-    arr.insert(index, 1, 2)
+  arr.insert(index, 1, 2)
 end
