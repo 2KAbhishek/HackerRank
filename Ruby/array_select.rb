c388870ce67025e01493a8ac72fc2ce8f12f09dd
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-01-05 16:47:31 +0530

    Add array_select.rb

--- Ruby/array_select.rb
+++ Ruby/array_select.rb
@@ -1,15 +1,15 @@
 def select_arr(arr)
-    arr.select {|a| a % 2 == 1}
+  arr.select(&:odd?)
 end
 
 def reject_arr(arr)
-    arr.reject {|a| a % 3 == 0}
+  arr.reject { |a| (a % 3).zero? }
 end
 
 def delete_arr(arr)
-    arr.delete_if {|a| a < 0}
+  arr.delete_if(&:negative?)
 end
 
 def keep_arr(arr)
-    arr.keep_if {|a| a >= 0}
+  arr.keep_if { |a| a >= 0 }
 end
