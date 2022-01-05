4534f854bad5392f7de4c3bddf1a9fad74c3f8e8
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-01-05 16:47:31 +0530

    Add methods_arguments.rb

--- Ruby/methods_arguments.rb
+++ Ruby/methods_arguments.rb
@@ -1,5 +1,6 @@
 def take(arr, len = 1)
-    size = arr.length
-    return [] if len >= size
-    arr[-(size - len) .. -1]
+  size = arr.length
+  return [] if len >= size
+
+  arr[-(size - len)..-1]
 end
